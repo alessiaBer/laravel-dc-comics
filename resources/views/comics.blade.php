9a3508a10171e0179c2d687bee1899aa32eb0372
alessiaBer <alexdeyn@gmail.com>
2023-05-31 16:07:22 +0200

    [add] banner

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -30,4 +30,37 @@
 
     </div>
 </section>
+<div class="banner bg-primary">
+    <div class="container my_container_lg text-white py-5">
+        <div class="row row-cols-1 row-cols-md-5">
+            <div class="col d-flex align-items-center">
+                <img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="" height="50px">
+                <span class="d-inline-block text-uppercase">digital comics</span>
+            </div>
+            <!-- /.col -->
+            <div class="col d-flex align-items-center">
+                <img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="" height="50px">
+                <span class="d-inline-block text-uppercase">dc merchandise</span>
+            </div>
+            <!-- /.col -->
+            <div class="col d-flex align-items-center">
+                <img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="" height="50px">
+                <span class="d-inline-block text-uppercase">subscription</span>
+            </div>
+            <!-- /.col -->
+            <div class="col d-flex align-items-center">
+                <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="" height="50px">
+                <span class="d-inline-block text-uppercase">comic shop locator</span>
+            </div>
+            <!-- /.col -->
+            <div class="col d-flex align-items-center">
+                <img src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="" width="60px">
+                <span class="d-inline-block text-uppercase">dc power visa</span>
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row row-cols-1 row-cols-md-5 -->
+    </div>
+    <!-- /.container my_container_lg -->
+</div>
 @endsection
