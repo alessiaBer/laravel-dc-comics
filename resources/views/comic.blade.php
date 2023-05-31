7976b89e840a4b553068f72cab5d4d7e7ae4a6c4
alessiaBer <alexdeyn@gmail.com>
2023-05-31 23:14:26 +0200

    [add] single comic style

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -17,15 +17,18 @@
     
     <div class="row">
         <div class="col-8">
-            <h2 class="text-uppercase">{{$comic->title}}</h2>
-            <div class="price-banner">
-                <div class="banner_left">
-                    <span class="price">U.S. Price: 
-                    <span class="text-white">{{$comic->price}}</span>
-                </span>
-                <span class="text-uppercase">Available</span>
+            <h2 class="text-uppercase pt-5">{{$comic->title}}</h2>
+            <div class="price-banner d-flex justify-content-between">
+                <div class="banner_left d-flex justify-content-between">
+                    <span class="price d-inline-block pe-1">
+                        <strong>U.S. Price: </strong> 
+                        <span class="text-white">{{$comic->price}}</span>
+                    </span>
+                    <span class="text-uppercase avail d-inline-block">
+                        <strong>Available</strong> 
+                    </span>
                 </div>
-                <span class="text-white">
+                <span class="text-white d-block check_avail">
                     Check Availability 
                     <i class="fa-solid fa-caret-down"></i>
                 </span>
@@ -35,7 +38,15 @@
             </div>
         </div>
         <div class="col-4">
-            <h4>ADVERTISEMENT</h4>
+            <div class="adv-container position-relative">
+               <h4 class="pt-4 text-end">ADVERTISEMENT</h4> 
+               <img src="https://cdn.vox-cdn.com/thumbor/deScZLd-0Y1LOe6ppjmpeA9YVtY=/1400x788/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22802887/IMG_DE272BF718E8_1.jpeg" alt="adv">
+               <span class="d-block position-absolute banner-title">UNLEASH <br>HEORIC<br> REWARDS</span>
+               <button type="button" class="btn btn-warning rounded-0 position-absolute">APPLY NOW</button>
+               <span class="visa d-block text-uppercase position-absolute">dc power visa</span>
+            </div>
+            
+
         </div>
     </div>
 </div>
