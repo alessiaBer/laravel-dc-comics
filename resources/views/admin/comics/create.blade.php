e8e276715948905790e8075065ad28d7c828a171
alessiaBer <alexdeyn@gmail.com>
2023-06-01 12:36:52 +0200

    [add] errors in create form

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -9,30 +9,45 @@
 
             <div class="col-12">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
                 @error('title')
-                    <div class="alert alert-danger">{{ $message }}</div>
+                    <small class="text-danger">{{ $message }}</small>
                 @enderror
             </div>
             <div class="col-12">
                 <label for="description" class="form-label">Description</label>
-                <textarea class="form-control" type="text" id="description" name="description" rows="3"></textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror" type="text" id="description" name="description" rows="3">{{ old('description') }}</textarea>
+                @error('description')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
             <div class="col-12">
                 <label for="thumb" class="form-label">Thumb</label>
-                <input type="text" class="form-control" id="thumb" name="thumb" placeholder="Image URL">
+                <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" placeholder="Image URL" value="{{ old('thumb') }}">
+                @error('thumb')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
-            <div class="col-md-6">
+            <div class="col-md-5">
                 <label for="price" class="form-label">Price</label>
-                <input type="text" class="form-control" id="price" name="price">
+                <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') }}">
+                @error('price')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
-            <div class="col-md-2">
+            <div class="col-md-3">
                 <label for="sale_date" class="form-label">Sale Date</label>
-                <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="YYYY-MM-DD">
+                <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" placeholder="YYYY-MM-DD" value="{{ old('sale_date') }}">
+                @error('sale_date')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
             <div class="col-md-4">
                 <label for="type" class="form-label">Type</label>
-                <input type="text" class="form-control" id="type" name="type" placeholder="ex. Comic Book">
+                <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" placeholder="ex. Comic Book" value="{{ old('type') }}">
+                @error('type')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
             <div class="col-md-4 mx-auto text-center pt-3">
                 <button type="submit" class="btn btn-light text-dark px-4">Sign in</button>
