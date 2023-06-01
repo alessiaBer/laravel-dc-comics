4c8f6183b2a63c6d704874497a32c051cae7bdff
alessiaBer <alexdeyn@gmail.com>
2023-06-01 12:05:43 +0200

    [add] validation in Request

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Comic;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreComicRequest;
+use App\Http\Requests\UpdateComicRequest;
 
 class ComicController extends Controller
 {
@@ -35,17 +37,10 @@ class ComicController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreComicRequest $request)
     {
-        $comic = new Comic();
-
-        $comic->title = $request->title;
-        $comic->description = $request->description;
-        $comic->thumb = $request->thumb;
-        $comic->price = $request->price;
-        $comic->sale_date = $request->sale_date;
-        $comic->type = $request->type;
-        $comic->save();
+        $val_data = $request->validated();
+        Comic::create($val_data);
 
         return to_route('comics.index')->with('message', 'Comic created successfully');
     }
@@ -79,18 +74,11 @@ class ComicController extends Controller
      * @param  \App\Models\Comic  $comic
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Comic $comic)
+    public function update(UpdateComicRequest $request, Comic $comic)
     {
-        $data = [
-            'title' => $request->title,
-            'description' => $request->description,
-            'thumb' => $request->thumb,
-            'price' => $request->price,
-            'sale_date' => $request->sale_date,
-            'type' => $request->type,
-        ];
+        $val_data = $request->validated();
 
-        $comic->update($data);
+        $comic->update($val_data);
 
         return to_route('comics.index')->with('message', 'Comic updated successfully');
     }
